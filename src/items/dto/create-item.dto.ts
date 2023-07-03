7a359dc1d08bc9a884ead3267f05079c58b6a208
engineer_tacky <tsubasa801@gmail.com>
2023-07-03 12:33:37 +0900

    class-validatorを使用してバリデーションを行う

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   readonly name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
   readonly price: number;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
 }
