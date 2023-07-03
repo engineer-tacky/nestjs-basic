05a518a3c98eb963c41c7ce3d6fa60362359097f
engineer_tacky <tsubasa801@gmail.com>
2023-07-03 09:33:49 +0900

    create追加

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
+
   findAll(): string {
     return 'Get all items';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
